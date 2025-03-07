88d4b5ee17e86df124b7aa11955e76824e2d682f
nairx <nairxprv@gmail.com>
2025-03-07 14:25:46 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App3 from "./App3";
+import App4 from "./App4";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App3 />);
+root.render(<App4 />);
