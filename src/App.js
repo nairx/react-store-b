4690467fa4e508750105d85edd45e1412db30bf1
nairx <nairxprv@gmail.com>
2025-03-19 12:12:22 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Cart from "./components/Cart";
 import Products from "./components/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App(props) {
   return (
-    <>
-      <Header/>
-      <Products/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Products />} />
+        <Route path="home" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 export default App;
