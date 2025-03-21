0582dcdad8603e2fcfd665552531f2595fc6c12b
nairx <nairxprv@gmail.com>
2025-03-21 11:58:53 +0530

    Updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,27 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import "./Register.css"
 import { Link } from "react-router-dom";
 export default function Register() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const handleSubmit = () => {
+    setUsers([...users, user]);
+  };
   return (
-    <div>
+    <div className="App-Register-Row">
       <div>
         <h2>Registration Form</h2>
         <p>
-          <input type="text" placeholder="Enter name"></input>
+          <input
+            type="text"
+            placeholder="Enter name"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          ></input>
         </p>
         <p>
-          <input type="text" placeholder="Email address"></input>
+          <input
+            type="text"
+            placeholder="Email address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          ></input>
         </p>
         <p>
-          <input type="password" placeholder="New password"></input>
+          <input
+            type="password"
+            placeholder="New password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          ></input>
         </p>
         <p>
-          <button>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
         </p>
         <p>
           <Link to="../login">Already a member? Log In</Link>
         </p>
       </div>
-      <div></div>
+      <div>
+        {
+          users && users.map((value,index)=>(
+            <li>{value.name}-{value.email}-{value.password}</li>
+          ))
+        }
+      </div>
     </div>
   );
 }
